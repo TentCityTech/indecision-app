7d8928a9dae1fc1f44f9e6554a24dd0ef308e47a
Tent City Tech <tentcitytech@TCT-Mac.local>
2020-11-09 15:23:23 -0800

    add style

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -62,10 +62,12 @@ class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle} />
+                <div className='container'>
                 <Action
                     hasOptions={this.state.options.length > 0}
                     handlePick={this.handlePick}
                 />
+                <div className='widget'>
                 <Options
                     options={this.state.options}
                     handleDeleteOptions={this.handleDeleteOptions}
@@ -74,6 +76,8 @@ class IndecisionApp extends React.Component {
                 <AddOption
                     handleAddOption={this.handleAddOption}
                 />
+                </div>  
+            </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
